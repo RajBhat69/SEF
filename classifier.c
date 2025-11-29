c27584eecd0643982b4a4220ebf1a831d89a8fc4
RajBhat69 <rbhat@conestogac.on.ca>
2025-11-29 01:14:57 -0500

    Fix: Add C-specific features and main function

--- classifier.c
+++ classifier.c
@@ -2,6 +2,23 @@
 
 void train() {
     printf("train\n");
+    printf("The second feature is added\n");
+}
+
+void predict() {
+    printf("predict\n");
+}
+
+int main() {
+    train();
+    predict();
+    return 0;
+}
+
+#include <stdio.h>
+
+void train() {
+    printf("train\n");
 }
 
 #include <stdio.h>
